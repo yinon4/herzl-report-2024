e9f8f2155699f1786279269f9ce52e243d955954
ינוק <u9268782@bsmch.net>
2024-11-20 16:04:39 +0200

    cool

--- scripts.js
+++ scripts.js
@@ -12,67 +12,65 @@ const backgrounds = [
   "bg10",
 ];
 
+let currentSlideIndex = 0;
+const slides = document.querySelectorAll(".slide");
+const showButtons = document.querySelectorAll(".show");
+const prevButton = document.querySelector(".prev");
+const nextButton = document.querySelector(".next");
+const body = document.querySelector("body");
+
+function setBackground() {
+  body.classList.remove(...backgrounds); // Remove all background classes
+  const randomIndex = Math.floor(Math.random() * backgrounds.length) + 1; // Get random index
+  body.classList.add(`bg${randomIndex}`); // Apply the random background class
+  randomizeVars(body);
+}
+
+// Show the current slide and hide others
+function showSlide(index) {
+  slides.forEach((slide, i) => {
+    if (i === index) {
+      slide.classList.add("active");
+      setBackground(slide);
+    } else {
+      slide.classList.remove("active");
+    }
+  });
+  reHideH1();
+}
+
+function randomizeVars() {
+  // colors
+  const randomNum = Math.floor(Math.random() * 360);
+  const cols = [0, 135, -135, 180].map((num) => (num + randomNum + 360) % 360);
+  const hslas = cols.map((val) => `hsl(${val}, 100%, 66%)`);
+  hslas.forEach((hsla, i) => {
+    body.style.setProperty(`--color${i + 1}`, hsla);
+  });
+
+  // angle
+  const angle = Math.random() * 360;
+  body.style.setProperty("--angle", `${angle}deg`);
+
+  // boxSize
+  const width = 100 / Math.floor(Math.random() * 50);
+  const height = 100 / Math.floor(Math.random() * 50);
+  body.style.setProperty("--width", `${width}%`);
+  body.style.setProperty("--height", `${height}%`);
+}
+
+function reHideH1() {
+  const h1s = document.querySelectorAll("h1");
+  h1s.forEach((h1) => {
+    h1.style.display = "none";
+  });
+
+  showButtons.forEach((button) => {
+    button.style.display = "inline-block";
+  });
+}
+
 document.addEventListener("DOMContentLoaded", function () {
-  let currentSlideIndex = 0;
-  const slides = document.querySelectorAll(".slide");
-  const showButtons = document.querySelectorAll(".show");
-  const prevButton = document.querySelector(".prev");
-  const nextButton = document.querySelector(".next");
-  const body = document.querySelector("body");
-
-  function setBackground() {
-    body.classList.remove(...backgrounds); // Remove all background classes
-    const randomIndex = Math.floor(Math.random() * backgrounds.length) + 1; // Get random index
-    body.classList.add(`bg${randomIndex}`); // Apply the random background class
-    randomizeVars(body);
-  }
-
-  // Show the current slide and hide others
-  function showSlide(index) {
-    slides.forEach((slide, i) => {
-      if (i === index) {
-        slide.classList.add("active");
-        setBackground(slide);
-      } else {
-        slide.classList.remove("active");
-      }
-    });
-    reHideH1();
-  }
-
-  function randomizeVars() {
-    // colors
-    const randomNum = Math.floor(Math.random() * 360);
-    const cols = [0, 135, -135, 180].map(
-      (num) => (num + randomNum + 360) % 360
-    );
-    const hslas = cols.map((val) => `hsl(${val}, 100%, 66%)`);
-    hslas.forEach((hsla, i) => {
-      body.style.setProperty(`--color${i + 1}`, hsla);
-    });
-
-    // angle
-    const angle = Math.random() * 360;
-    body.style.setProperty("--angle", `${angle}deg`);
-
-    // boxSize
-    const width = 100 / Math.floor(Math.random() * 50);
-    const height = 100 / Math.floor(Math.random() * 50);
-    body.style.setProperty("--width", `${width}%`);
-    body.style.setProperty("--height", `${height}%`);
-  }
-
-  function reHideH1() {
-    const h1s = document.querySelectorAll("h1");
-    h1s.forEach((h1) => {
-      h1.style.display = "none";
-    });
-
-    showButtons.forEach((button) => {
-      button.style.display = "inline-block";
-    });
-  }
-
   // Show/hide the h1 text when the "הראה" button is clicked
   showButtons.forEach((button) => {
     button.addEventListener("click", function () {
